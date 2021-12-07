ba39bb8b617e82930b386ab5fe74599ce1ab851f
Pujiyanto1024 <pujiyanto2650@gmail.com>
2021-12-07 17:19:23 +0700

    Finish section 7

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -13,7 +13,7 @@ router.post("/api/users/signup", [
     .trim()
     .isLength({min: 4, max: 10})
     .withMessage("Password must be between 4 - 10 characters")
-], (req: Request, res: Response) => {
+], async(req: Request, res: Response) => {
     const errors = validationResult(req);
 
     if(!errors.isEmpty()) {
