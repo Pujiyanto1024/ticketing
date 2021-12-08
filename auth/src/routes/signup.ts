cc4db0380830748c9e605f4114b3bdabd5fa15c0
Pujiyanto1024 <pujiyanto2650@gmail.com>
2021-12-08 16:21:23 +0700

    Section 9, video 178

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
+import jwt from "jsonwebtoken";
+
 import { RequestValidationError } from "../errors/request-validation-error";
 import { DbConnectionError } from "../errors/db-connection-error";
 import { BadRequestError } from "../errors/bad-request-error";
@@ -30,8 +32,18 @@ router.post("/api/users/signup", [
     }
 
     const user = User.build({ email, password });
-
     await user.save();
+
+    //Generate JWT
+    const userJwt = jwt.sign({
+      id: user.id,
+      email: user.email
+    }, process.env.JWT_KEY!);
+
+    //Store it on session
+    req.session = {
+      jwt: userJwt
+    }
 
     res.status(201).send(user);
 });
